e8e7f579b0ff7cd3e44d933723294bae3e99d58a
Martin Brekhof <martin@brekhof.nl>
2023-09-27 19:30:56 +0200

    client app

---
+++ mbGPT/Program.cs
@@ -0,0 +1,15 @@
+﻿using mbGPT;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddDevExpressBlazor(options => {
+    options.BootstrapVersion = DevExpress.Blazor.BootstrapVersion.v5;
+    options.SizeMode = DevExpress.Blazor.SizeMode.Medium;
+});
+
+await builder.Build().RunAsync();
